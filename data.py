c00911c66320c30acdacd1ca3e7bac551f369d1d
Theo Long <theodorelong98@gmail.com>
2023-02-04 22:12:39 +0000

    allow any datasets, train/val/test split

--- data.py
+++ data.py
@@ -1,11 +1,27 @@
 from torch_geometric.datasets import TUDataset
-from torch_geometric.data import DataLoader
+from torch_geometric.loader import DataLoader
+
+import math
+
+TRAIN_SPLIT = 0.6
+VAL_SPLIT = 0.2
+TEST_SPLIT = 0.2
 
 
-def get_protein_dataset():
-    dataset = TUDataset(root="./datasets", name="PROTEIN", use_node_attr=True)
+def get_tu_dataset(name):
+    dataset = TUDataset(root="./datasets", name=name, use_node_attr=True)
     return dataset
 
 
-def generate_dataloader(dataset, batch_size):
-    return DataLoader(dataset, batch_size=batch_size, shuffle=True)
+def generate_dataloaders(dataset: TUDataset, batch_size):
+    # random shuffle before splitting
+    dataset.shuffle()
+    loaders = []
+    index = 0
+    for split in [TRAIN_SPLIT, VAL_SPLIT, TEST_SPLIT]:
+        upper_index = index + math.ceil(len(dataset) * split)
+        loaders.append(
+            DataLoader(dataset[index:upper_index], batch_size=batch_size, shuffle=True)
+        )
+        index = upper_index
+    return tuple(loaders)
