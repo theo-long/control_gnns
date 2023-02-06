88297251423bc4d70879c34c184f66667a46cd59
Theo Long <theodorelong98@gmail.com>
2023-02-06 14:26:45 +0000

    handle new model hyperparameters, allow for multiple runs

--- run_experiment.py
+++ run_experiment.py
@@ -1,20 +1,24 @@
 from training import train_eval, TrainConfig, BasicLogger
 from data import get_tu_dataset, generate_dataloaders
-from models import BasicGCN
+from models import BasicGCN, LinearGCN, GraphMLP
 
 import argparse
 import wandb
 from torch.nn.functional import cross_entropy
 from torchmetrics import Accuracy
 
+import pandas as pd
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset", default="PROTEINS")
-    parser.add_argument("-n", "--name", default=None)
+    parser.add_argument("-n", "--num_runs", default=1)
     parser.add_argument("--model", required=True)
     parser.add_argument("--hidden_dim", default=64, type=int)
-    parser.add_argument("--num_layers", default=2, type=int)
+    parser.add_argument("--num_encoding_layers", default=1, type=int)
+    parser.add_argument("--num_decoding_layers", default=1, type=int)
+    parser.add_argument("--num_conv_layers", default=2, type=int)
     parser.add_argument("--dropout", default=0.0, type=float)
     parser.add_argument("--lr", default=0.001, type=float)
     parser.add_argument("--epochs", default=10, type=int)
@@ -41,37 +45,70 @@ def main():
             input_dim=dataset[0].x.shape[1],
             output_dim=dataset.num_classes,
             hidden_dim=args.hidden_dim,
-            num_layers=args.num_layers,
+            num_conv_layers=args.num_conv_layers,
+            num_decoding_layers=args.num_decoding_layers,
+            num_encoding_layers=args.num_encoding_layers,
+            dropout_rate=args.dropout,
+        )
+    elif args.model.lower() == "linear_gcn":
+        model = LinearGCN(
+            input_dim=dataset[0].x.shape[1],
+            output_dim=dataset.num_classes,
+            hidden_dim=args.hidden_dim,
+            num_conv_layers=args.num_conv_layers,
+            num_decoding_layers=args.num_decoding_layers,
+            num_encoding_layers=args.num_encoding_layers,
+            dropout_rate=args.dropout,
+        )
+    elif args.model.lower() == "mlp":
+        model = GraphMLP(
+            input_dim=dataset[0].x.shape[1],
+            output_dim=dataset.num_classes,
+            hidden_dim=args.hidden_dim,
+            num_decoding_layers=args.num_decoding_layers,
+            num_encoding_layers=args.num_encoding_layers,
             dropout_rate=args.dropout,
         )
     else:
         raise ValueError(f"Model name {args.model} not recognized")
 
-    if not args.debug:
-        wandb.init(project="control_gnns", config=training_config)
-        wandb.config.model = args.model
-        wandb.config.hidden_dim = args.hidden_dim
-        wandb.config.num_layers = args.num_layers
-        wandb.config.dropout_rate = args.dropout
-        if args.name:
-            wandb.run.name = args.name
-        logger = wandb
-    else:
-        logger = BasicLogger()
+    stats = []
+    for run in range(args.num_runs):
+        print(f"Starting run {run}", end="\r")
+        if not args.debug:
+            run = wandb.init(
+                project="control_gnns", config=training_config, reinit=True
+            )
+            wandb.config.model = args.model
+            wandb.config.hidden_dim = args.hidden_dim
+            wandb.config.num_layers = args.num_layers
+            wandb.config.dropout_rate = args.dropout
+            if args.name:
+                wandb.run.name = args.name
+            logger = run
+        else:
+            logger = BasicLogger()
 
-    accuracy_function = Accuracy("multiclass", num_classes=dataset.num_classes)
-    final_stats = train_eval(
-        model,
-        training_config,
-        train_loader,
-        val_loader,
-        test_loader,
-        loss_function=cross_entropy,
-        metric_function=accuracy_function,
-        logger=logger,
+        accuracy_function = Accuracy("multiclass", num_classes=dataset.num_classes)
+        final_stats = train_eval(
+            model,
+            training_config,
+            train_loader,
+            val_loader,
+            test_loader,
+            loss_function=cross_entropy,
+            metric_function=accuracy_function,
+            logger=logger,
+        )
+        stats.append(final_stats)
+
+    stats_df = pd.DataFrame(stats)
+    mean_stats = stats_df.mean().to_dict()
+    std_stats = stats_df.std().to_dict()
+
+    print(
+        f"Training completed, mean final stats:\n {mean_stats} \n std_devs:{std_stats}"
     )
-
-    print(f"Training completed, final stats:\n {final_stats}")
 
 
 if __name__ == "__main__":
