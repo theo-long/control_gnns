12ac7f8836c73c39d6f6f89fd03cfc2e1378fd37
Theo Long <theodorelong98@gmail.com>
2023-02-17 22:06:34 +0000

    new hyperparameter tuning script

--- hyperparameter_tuning.py
+++ hyperparameter_tuning.py
@@ -1,8 +1,13 @@
 import argparse
 from dataclasses import dataclass, asdict
 from typing import Dict, List, Any, Union
+from training import train_eval, TrainConfig
+from models import GraphMLP, GCN
+from data import generate_dataloaders, get_tu_dataset
 
 import wandb
+import torchmetrics
+from torch.nn.functional import cross_entropy
 
 
 @dataclass
@@ -42,29 +47,116 @@ mlp_sweep = SweepConfiguration(
         "weight_decay": ContinuousParameter(
             0.01, 1e-7, distribution="log_uniform_values"
         ),
+        "beta1": ContinuousParameter(0.5, 0.99, distribution="log_uniform_values"),
+        "dropout_rate": ContinuousParameter(0.5, 0.0, distribution="uniform"),
+    },
+)
+
+gcn_sweep = SweepConfiguration(
+    method="random",
+    name="gcn",
+    metric={"name": "val_loss", "goal": "minimize"},
+    parameters={
+        "lr": ContinuousParameter(0.1, 0.00001, distribution="log_uniform_values"),
+        "weight_decay": ContinuousParameter(
+            0.01, 1e-7, distribution="log_uniform_values"
+        ),
+        "beta1": ContinuousParameter(0.5, 0.99, distribution="log_uniform_values"),
         "dropout_rate": ContinuousParameter(0.5, 0.0, distribution="uniform"),
     },
 )
 
 
-SWEEPS_DICT = {"mlp": mlp_sweep}
+SWEEPS_DICT = {
+    "mlp": mlp_sweep,
+    "gcn": gcn_sweep,
+}
 
 
-def single_training_run():
-    pass
+def training_run_factory(model_factory, epochs: int, dataset, batch_size=128):
+    """Wrapper function to generate a function that trains a single model.
+    Needed so we can dynamically generate a target function for different datasets.
+
+    Args:
+        model_factory: creates the model object
+        epochs (int): number of epochs to train for
+        dataset: dataset to train on
+        batch_size (int, optional): batch size. Defaults to 128.
+    """
+    train_loader, val_loader, test_loader = generate_dataloaders(dataset, batch_size)
+
+    def single_training_run():
+        hyperparameters = dict(wandb.config)
+        training_config = TrainConfig(
+            lr=hyperparameters.pop("lr"),
+            batch_size=batch_size,
+            epochs=epochs,
+            weight_decay=hyperparameters.pop("weight_decay"),
+        )
+        accuracy_function = torchmetrics.Accuracy(
+            "multiclass", num_classes=dataset.num_classes
+        )
+        model = model_factory(**hyperparameters)
+        final_stats = train_eval(
+            model,
+            training_config,
+            train_loader,
+            val_loader,
+            test_loader,
+            loss_function=cross_entropy,
+            metric_function=accuracy_function,
+            logger=wandb,
+        )
+        return final_stats
+
+    return single_training_run
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", "--name", required=True)
+    parser.add_argument("-t", "--time_inv", action="store_true", default=False)
+    parser.add_argument("-l", "--linear", action="store_true", default=False)
+    parser.add_argument("--num_encoding_layers", default=2, type=int)
+    parser.add_argument("--num_decoding_layers", default=2, type=int)
+    parser.add_argument("--num_conv_layers", default=2, type=int)
     parser.add_argument("--dataset", default="PROTEINS")
     parser.add_argument("-m", "--max_runs", default=10)
     args = parser.parse_args()
 
     sweep_configuration = SWEEPS_DICT[args.name]
     sweep_id = wandb.sweep(sweep=sweep_configuration, project="control_gnns")
+    dataset = get_tu_dataset(args.dataset)
 
-    wandb.agent(sweep_id=sweep_id, function=single_training_run, count=args.max_runs)
+    if args.name == "mlp":
+        model_factory = lambda dropout_rate: GraphMLP(
+            input_dim=dataset[0].x.shape[1],
+            output_dim=dataset.num_classes,
+            hidden_dim=args.hidden_dim,
+            num_decoding_layers=args.num_encoding_layers,
+            num_encoding_layers=args.num_decoding_layers,
+            dropout_rate=dropout_rate,  # passed during hyperparameter tuning
+        )
+    elif args.name == "gcn":
+        model_factory = lambda dropout_rate: GCN(
+            input_dim=dataset[0].x.shape[1],
+            output_dim=dataset.num_classes,
+            hidden_dim=args.hidden_dim,
+            num_conv_layers=args.num_conv_layers,
+            num_decoding_layers=args.num_encoding_layers,
+            num_encoding_layers=args.num_decoding_layers,
+            dropout_rate=dropout_rate,  # passed during hyperparameter tuning
+            linear=args.linear,
+            time_inv=args.time_inv,
+        )
+    else:
+        raise ValueError(f"Model name {args.name} not recognized")
+
+    training_function = training_run_factory(
+        epochs=50, model_factory=model_factory, dataset=dataset
+    )
+
+    wandb.agent(sweep_id=sweep_id, function=training_function, count=args.max_runs)
 
 
 if __name__ == "__main__":
