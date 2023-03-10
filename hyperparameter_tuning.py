ef502f21a40e635fef838b084f399bc47f1ea1af
Theo Long <theodorelong98@gmail.com>
2023-03-10 19:54:49 +0000

    update hyperparam script

--- hyperparameter_tuning.py
+++ hyperparameter_tuning.py
@@ -5,7 +5,7 @@ from training import train_eval, TrainConfig
 from models import GraphMLP, GCN
 from data import generate_dataloaders, get_tu_dataset, get_test_val_train_split
 from control import CONTROL_DICT
-from utils import get_device
+from utils import get_device, parse_callable_string
 
 import wandb
 import torchmetrics
@@ -67,23 +67,41 @@ def main():
     parser.add_argument("-e", "--epochs", default=20, type=int)
     parser.add_argument("-m", "--model", required=True)
 
-    parser.add_argument("--control_type", default="null", type=str)
-    parser.add_argument("--control_stat", default="degree", type=str)
-    parser.add_argument("--control_k", default=1, type=int)
-    parser.add_argument("--control_normalise", action="store_true")
+    parser.add_argument(
+        "--control_type", default="null", type=str, choices=["null", "gcn", "mp"]
+    )
+    parser.add_argument(
+        "--control_edges", default="adj", type=str, choices=["adj", "dense"]
+    )
+    parser.add_argument(
+        "--control_metric",
+        default="b_centrality",
+        type=str,
+        choices=["degree", "b_centrality"],
+    )
+    parser.add_argument("--control_k", default=lambda x: 1, type=parse_callable_string)
 
     parser.add_argument("-t", "--time_inv", action="store_true", default=False)
     parser.add_argument("-l", "--linear", action="store_true", default=False)
     parser.add_argument("--hidden_dim", default=128, type=int)
-    parser.add_argument("--num_encoding_layers", default=2, type=int)
-    parser.add_argument("--num_decoding_layers", default=2, type=int)
     parser.add_argument("--conv_depth", default=2, type=int)
+
     parser.add_argument("--dataset", default="PROTEINS")
+
     args = parser.parse_args()
 
     sweep_configuration = SWEEPS_DICT[args.model]
     sweep_configuration.name = args.name
     sweep_id = wandb.sweep(sweep=sweep_configuration.to_dict(), project="control_gnns")
+
+    dataset = get_tu_dataset(
+        args.dataset,
+        args.control_type,
+        args.control_edges,
+        args.control_metric,
+        args.control_k,
+    )
+
     dataset = get_tu_dataset(args.dataset)
     splits = get_test_val_train_split(args.dataset, seed=0)
     train_loader, val_loader, test_loader = generate_dataloaders(
@@ -95,29 +113,18 @@ def main():
             input_dim=dataset[0].x.shape[1],
             output_dim=dataset.num_classes,
             hidden_dim=args.hidden_dim,
-            num_decoding_layers=args.num_encoding_layers,
-            num_encoding_layers=args.num_decoding_layers,
-            dropout_rate=dropout_rate,  # passed during hyperparameter tuning
+            dropout_rate=args.dropout,
         )
     elif args.model == "gcn":
-        control_factory = lambda: CONTROL_DICT[args.control_type](
-            feature_dim=args.hidden_dim,
-            node_stat=args.control_stat,
-            k=args.control_k,
-            normalise=args.control_normalise,
-        )
-
         model_factory = lambda dropout_rate: GCN(
             input_dim=dataset[0].x.shape[1],
             output_dim=dataset.num_classes,
             hidden_dim=args.hidden_dim,
             conv_depth=args.conv_depth,
-            control_factory=control_factory,
-            num_decoding_layers=args.num_encoding_layers,
-            num_encoding_layers=args.num_decoding_layers,
             dropout_rate=dropout_rate,  # passed during hyperparameter tuning
             linear=args.linear,
             time_inv=args.time_inv,
+            control_type=args.control_type,
         )
     else:
         raise ValueError(f"Model name {args.model} not recognized")
