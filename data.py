f8157555fc643b16002f5c51d8bcd62a2e85ff4d
Theo Long <theodorelong98@gmail.com>
2023-03-11 16:17:45 +0000

    fix warning for using ints for indexing

--- data.py
+++ data.py
@@ -180,9 +180,9 @@ def get_test_val_train_mask(
     dataset: Union[WikipediaNetwork, Planetoid], split: int = 0
 ):
     return (
-        dataset[0].train_mask[:, split],
-        dataset[0].val_mask[:, split],
-        dataset[0].test_mask[:, split],
+        dataset[0].train_mask[:, split].to(torch.bool),
+        dataset[0].val_mask[:, split].to(torch.bool),
+        dataset[0].test_mask[:, split].to(torch.bool),
     )
 
 
