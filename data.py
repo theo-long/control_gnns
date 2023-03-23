896f71dfca5c204eef6554c8d0f5f3ca34b22bc4
Theo Long <theodorelong98@gmail.com>
2023-03-23 15:30:50 -0400

    fix

--- data.py
+++ data.py
@@ -334,7 +334,7 @@ def get_dataset(
     if "sbm" in name:
         pre_transforms.append(StochasticBlockModelTransform())
 
-    if "linear" or "tree" or "labelprop" in name:
+    if ("linear" in name) or ("tree" in name) or ("labelprop" in name):
         # synthetic datasets have no pre-transform
         transforms.append(RankingTransform())
     else:
